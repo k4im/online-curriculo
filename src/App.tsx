4b7b322ad1e34540e125b13f0b7f3e67afb51e1a
João Victor <joao@platon.com.br>
2024-07-22 14:35:14 -0300

    chore: first commit.

---
+++ src/App.tsx
@@ -0,0 +1,12 @@
+import { Hero } from "./application/layout/hero/hero"
+
+function App() {
+
+  return (
+    <>
+    <Hero></Hero>
+    </>
+  )
+}
+
+export default App
---
+++ src/application/layout/hero/hero.tsx
@@ -0,0 +1,10 @@
+import './hero.css'
+export function Hero(){
+    return <>
+        <section className="hero bg-red-500 flex justify-center items-center">
+            <div className="container-left">
+
+            </div>
+        </section>
+    </>
+}
