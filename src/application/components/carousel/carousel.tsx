a90e96bed998431e9b935b76fd7dd44a99d575ca
João Victor <joao@platon.com.br>
2024-07-24 15:27:07 -0300

    feat: adicionado carousel.

---
+++ src/application/components/carousel/carousel.tsx
@@ -0,0 +1,56 @@
+import { useLayoutEffect, useRef } from "react";
+import gsap from 'gsap';
+import './carousel.css';
+
+export function Carousel()
+{
+    const wrapperRef = useRef(null);
+    useLayoutEffect(() => {
+        let ctx = gsap.context(() => {
+          animateCarrousel(wrapperRef);
+        },
+          wrapperRef
+        );
+    
+        return () => ctx.revert();
+      }, [])
+    return <>
+      <div id='carousel' className="overflow-hidden carrousel" ref={wrapperRef}>
+        <div className="wrapper">
+          <p className="carrousel-text text-white text-center">*</p>
+          <p className="carrousel-text text-white text-center">Asp.NetCore</p>
+          <p className="carrousel-text text-white text-center">*</p>
+          <p className="carrousel-text text-white text-center">Node.Js</p>
+          <p className="carrousel-text text-white text-center">*</p>
+          <p className="carrousel-text text-white text-center">Angular</p>
+          <p className="carrousel-text text-white text-center">*</p>
+          <p className="carrousel-text text-white text-center">React</p>
+          <p className="carrousel-text text-white text-center">*</p>
+          <p className="carrousel-text text-white text-center">Docker</p>
+          <p className="carrousel-text text-white text-center">*</p>
+          <p className="carrousel-text text-white text-center">Linux</p>
+          <p className="carrousel-text text-white text-center">*</p>
+          <p className="carrousel-text text-white text-center">CI/CD</p>
+          <p className="carrousel-text text-white text-center">*</p>
+          <p className="carrousel-text text-white text-center">Rust</p>
+          <p className="carrousel-text text-white text-center">*</p>
+        </div>
+      </div>    
+    </>
+}
+
+function animateCarrousel(wrapperRef: any) {
+    const wrapper = wrapperRef.current;
+    const slides = Array.from(wrapper.children);
+    //@ts-ignore
+    const slidesCopy = [...slides].map(el => el.cloneNode(true));
+    slidesCopy.forEach(el => wrapper.appendChild(el));
+    //@ts-ignore
+    const slideWidth = slides[0].offsetWidth; // Assuming all slides have the same width
+    const totalWidth = slides.length * slideWidth;
+  
+    const carouselTL = gsap.timeline({ repeat: -1 });
+    carouselTL.fromTo('.wrapper', { x: totalWidth * 0.2 }, { x: -totalWidth * 0.3, duration: 20, ease: 'linear' })
+    .totalProgress(0.5);
+  
+}
--- src/application/layout/hero/hero.tsx
+++ src/application/layout/hero/hero.tsx
@@ -3,11 +3,10 @@ import { NavBar } from "../../components/navbar/navbar";
 import { About } from "../about/about";
 import './hero.css'
 import gsap from 'gsap';
+import { Carousel } from "../../components/carousel/carousel";
 
-export function Hero()
-{
+export function Hero() {
   const comp = useRef(null);
-
   useLayoutEffect(() => {
     let ctx = gsap.context(() => {
       animateIcons();
@@ -15,28 +14,29 @@ export function Hero()
       animateSlogan();
       animateButton();
       animateScroll();
+      showCarousel();
     },
       comp
     );
 
     return () => ctx.revert();
   }, [])
-    return<>
-     <section ref={comp} id='hero-section' className='z-10 bg-gradient-to-r from-gray-950 to-gray-900 h-screen w-screen flex flex-col justify-start gap-64 items-center'>
-        <NavBar></NavBar>
-        
-        <div id='slogan' className="w-3/5 h-1/2 flex flex-col gap-5 items-center justify-center overflow-hidden">
-          <div className="hero-slogan overflow-hidden flex flex-col items-center">
-            <h2 className='text-white text-center h2-container'>
-              <span className='hero-h2'>Desenvolvedor de software full stack.</span>
-            </h2>
-            <p className='text-white text-3xl text-center p-container overflow-hidden mt-10'>
-              <span className='text-3xl hero-slogan-p'>Impulsionando empresas com tecnologia.</span>
-            </p>
-            <a
-              href='https://drive.google.com/file/d/1UsnzUHrILF03M3sRgS8ZdGZgff78kUGU/view'
-              type="button"
-              className="
+  return <>
+    <section ref={comp} id='hero-section' className='z-10 bg-gradient-to-r from-gray-950 to-gray-900 h-screen w-screen flex flex-col justify-start gap-64 items-center'>
+      <NavBar></NavBar>
+      <Carousel></Carousel>
+      <div id='slogan' className="w-3/5 h-1/2 flex flex-col gap-5 items-center justify-center overflow-hidden">
+        <div className="hero-slogan overflow-hidden flex flex-col items-center">
+          <h2 className='text-white text-center h2-container'>
+            <span className='hero-h2'>Desenvolvedor de software full stack.</span>
+          </h2>
+          <p className='text-white text-3xl text-center p-container overflow-hidden mt-10'>
+            <span className='text-3xl hero-slogan-p'>Impulsionando empresas com tecnologia.</span>
+          </p>
+          <a
+            href='https://drive.google.com/file/d/1UsnzUHrILF03M3sRgS8ZdGZgff78kUGU/view'
+            type="button"
+            className="
                 btn
                 text-white 
                 hover:bg-gradient-to-l 
@@ -53,20 +53,20 @@ export function Hero()
                 justify-center
                 items-center
                 "
-              style={{ background: "linear-gradient(100deg, #F9A8D4, #EC4899)" }}
-            >
-              <p className='text-lg text-center text-white'>Baixar Curriculo</p>
-            </a>
-          </div>
+            style={{ background: "linear-gradient(100deg, #F9A8D4, #EC4899)" }}
+          >
+            <p className='text-lg text-center text-white'>Baixar Curriculo</p>
+          </a>
         </div>
+      </div>
 
-        <div id='scroller' className="scroll-container">
-          <div className="scroll-ball"></div>
-        </div>
+      <div id='scroller' className="scroll-container">
+        <div className="scroll-ball"></div>
+      </div>
 
-        <About></About>
-      </section>
-    </>
+      <About></About>
+    </section>
+  </>
 }
 
 /**
@@ -180,4 +180,8 @@ function animateScroll() {
     repeat: -1,
     ease: "power1.inOut",
   })
+}
+function showCarousel() {
+  const carouselTL = gsap.timeline();
+  carouselTL.fromTo('#carousel', {opacity: 0}, {opacity: 1, duration: 1.5, delay: 6.8});
 }
