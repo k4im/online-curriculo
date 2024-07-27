b2d0a1d2bfc30d12daae1f60cc00411677772a24
João Victor <joao@platon.com.br>
2024-07-27 15:30:07 -0300

    refactor: removido white ball reveal.

--- src/application/layout/experience/experience.tsx
+++ src/application/layout/experience/experience.tsx
@@ -10,15 +10,13 @@ export function Experience() {
     const xpBallSection = useRef(null);
     return <>
         <section ref={xpBallSection} id='xp' className="hidden h-screen flex flex-col items-center w-screen overflow-hidden">
-            <div className="ball-reveal">
-            </div>
         </section>
         <section id='container-xp' className="hidden flex justify-start items-center container-experience h-screen w-screen">
-            <div className="container-xp h-full w-5/6 flex mt-14">
+            <div className="container-xp h-full w-5/6 flex">
                 <div id='languages'className="h-1/2 w-full flex flex-col gap-10">
                     <div id='javascript' className=" h-full w-full flex flex-col justify-start items-start gap-2">
                             <h3 className='text-purple-500 h-xp-profission'>Javascript</h3>
-                            <p className='text-zinc-900 p-xp-profission '>
+                            <p className='text-white p-xp-profission '>
                                 Ao criar aplicativos JavaScript, estou equipado com as ferramentas certas e posso funcionar de forma totalmente independente delas para fornecer soluções rápidas e resilientes, 
                                 otimizadas para escalabilidade — desempenho e escalabilidade são prioridades no meu radar.
                             </p>
@@ -29,7 +27,7 @@ export function Experience() {
                     </div>
                     <div id='aspnet' className=" h-full w-full flex flex-col justify-end items-end gap-2">
                             <h3 className='text-purple-500 h-xp-profission'>Asp.netCore</h3>
-                            <p className='text-zinc-900 p-xp-profission-left'>
+                            <p className='text-white p-xp-profission-left'>
                                 Ao criar aplicativos JavaScript, estou equipado com as ferramentas certas e posso funcionar de forma totalmente independente delas para fornecer soluções rápidas e resilientes, 
                                 otimizadas para escalabilidade — desempenho e escalabilidade são prioridades no meu radar.
                             </p>
@@ -50,15 +48,12 @@ function animateBallReveal() {
         scrollTrigger: {
             trigger: '#xp',
             scrub: true,
-            start: '240px 600px',
+            start: '150px 550px',
             end: '600px, 600px',
         },
         delay: 12,
         smoothChildTiming: true
     });
-    ballTL1.to('.ball-reveal', { opacity: 1, y: 350, delay: 3000, duration: 250000, ease: 'power1.inOut' })
-    ballTL1.to('.ball-reveal',
-            { opacity: 1, scale: 70, duration: 550000, delay: 2500, ease: 'power1.inOut' })
-    ballTL1.to('#container-xp', { opacity: 1, duration: 550000, delay: 2500, ease: 'power1.inOut' })
-    ballTL1.to('.xp-h2', { opacity: 1, duration: 1.8, delay: 0.5, ease: 'power1.inOut' })
+    ballTL1.to('#container-xp', { opacity: 1, y: 0, duration: 5, delay: 6, ease: 'power1.inOut' })
+    ballTL1.to('.xp-h2', { opacity: 1, duration: 5, delay: 0.5, ease: 'power1.inOut' })
 }
